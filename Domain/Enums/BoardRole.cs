a53d91a3b6242f0248da3247824e571e18bf6a00
Galen Veselinov <galenveselinov3@gmail.com>
2025-08-12 23:49:29 +0300

    Created the domain layer
    Created the ApplicationUser, Board, BoardMember, Card, CardLabel, Column, Comment, Label, Project, Project Member entities and coresponding enums.

---
+++ Domain/Enums/BoardRole.cs
@@ -0,0 +1,7 @@
+namespace Domain.Enums;
+
+public enum BoardRole
+{
+    Member = 1,
+    Admin = 2
+} 
---
+++ Domain/Enums/ProjectRole.cs
@@ -0,0 +1,8 @@
+namespace Domain.Enums;
+
+public enum ProjectRole
+{
+    Member = 1,
+    Admin = 2,
+    Owner = 3
+} 
